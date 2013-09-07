d266a012ce530c358da25d4eb407ebd5ecee0665
matthew-was <Matthew@h121156.ncnr.nist.gov>
2013-09-07 13:47:43 -0400

    Added a second slider for the detector distance. Determined reporting of Instrument, Number of Guides, and Sample to Detector Distance to the general variables.

--- public_html/js/scripts/guideslider.js
+++ public_html/js/scripts/guideslider.js
@@ -1,51 +1,118 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+    
     var usefulVars = {
-        Instr:"empty"
+        Instr:"NG3",
+        guides:1,
+        detector:400
     };
-
-    $('input[name="radio"]:checked').val();
+    
+    function getVariables() {
+        usefulVars["Instr"]= $('input[name="radio"]:checked').val();
+        usefulVars["guides"]= $( "#guides-slider" ).slider( "value" );
+        usefulVars["detector"]= $( "#detector-slider" ).slider( "value" );
+    };
 
     function handleClick(myRadio) {
         usefulVars["Instr"] = $('input[name="radio"]:checked').val();
         var result = $("#select-result").empty();
         result.append($('input[name="radio"]:checked').val());
-//        switch(usefulVars["Instr"])
-//            {
-//                case "NG3":
-//                    result.append("NG3");
-//                break;
-//                case "NG7":
-//                    result.append("NG7");
-//                break;
-//                case "NGB":
-//                    result.append("NGB");
-//                break;
-//                default:
-//                console.log("Something went wrong");
-//        }
-//        console.log("result = " + result);
-      
+        resetSlider();
+        resetDetector();
     }
-
-    $(function() {
-        $( "#slider-range" ).slider({
-        range: "min",
-        value: 5,
-        min: 0,
-        max: 8,
-            slide: function( event, ui ) {
-                $( "#guides" ).val( ui.value );
-            }
-        });
-        $( "#guides" ).val( $( "#slider-range" ).slider( "value" ) );
-    });
-
-
+    
     $(function(){
             $("#radio").buttonset();
     });
+
+    $(function() {
+        $( "#guides-slider" ).slider({        
+        value: 1,
+        min: 0,
+        max: 8,
+        slide: function( event, ui ) {
+            $( "#guides" ).val( ui.value );
+        },
+        change: function(event, ui) {
+            getVariables();
+        }
+        });
+        $( "#guides" ).val( $( "#guides-slider" ).slider( "value" ) );
+    });
+
+    function resetSlider() {
+        getVariables ();
+        var guideVal;
+        switch (usefulVars["Instr"]) {
+            case "NG3":
+                guideVal = 8;
+                break;
+            case "NG7":
+                guideVal = 8;
+                break;
+            case "NGB":
+                guideVal = 2;
+                break;
+            default:
+                console.log("Something went wrong");
+        }
+        $("#guides-slider").slider({
+            value:1,
+            max:guideVal,
+            slide: function( event, ui ) {
+                $( "#guides" ).val( ui.value );
+            },
+            change: function(event, ui) {
+                getVariables();
+            }
+        });
+        $( "#guides" ).val( $( "#guides-slider" ).slider( "value" ) );
+    }
+    
+       $(function() {
+        $( "#detector-slider" ).slider({        
+        range: "min",
+        value: 400,
+        min: 133,
+        max: 1377, 
+            slide: function( event, ui ) {
+                $( "#detector" ).val( ui.value );
+            },
+            change: function(event, ui) {
+                getVariables();
+            }
+        });
+        $( "#detector" ).val( $( "#detector-slider" ).slider( "value" ) );
+    });
+
+   function resetDetector() {
+       getVariables ();
+       var detMax;
+       var detMin;
+        switch (usefulVars["Instr"]) {
+            case "NG3":
+                detMin = 133;
+                detMax = 1377;
+                break;
+            case "NG7":
+                detMin = 100;
+                detMax = 1531;
+                break;
+            case "NGB":
+                detMin = 106;
+                detMax = 525;
+                break;
+            default:
+                console.log("Something went wrong");
+        }
+        $("#detector-slider").slider({
+            value:400,
+            min:detMin,
+            max:detMax,
+             slide: function( event, ui ) {
+                $( "#detector" ).val( ui.value );
+            },
+            change: function(event, ui) {
+                getVariables();
+            }
+        });
+        $( "#detector" ).val( $( "#detector-slider" ).slider( "value" ) );
+  }
