632390ccbe361ef8b27814d8bea305e85e449827
Matthew <Matthew@192.168.1.14>
2013-09-07 09:51:54 -0400

    Tweaked instrument reporting.

--- public_html/js/scripts/guideslider.js
+++ public_html/js/scripts/guideslider.js
@@ -12,22 +12,23 @@
 
     function handleClick(myRadio) {
         usefulVars["Instr"] = $('input[name="radio"]:checked').val();
-        var result = $("#select-result").empty();   
-        switch(usefulVars["Instr"])
-            {
-                case "NG3":
-                    result.append("NG3");
-                break;
-                case "NG7":
-                    result.append("NG7");
-                break;
-                case "NGB":
-                    result.append("NGB");
-                break;
-                default:
-                console.log("Something went wrong");
-        }
-        console.log("result = " + result);
+        var result = $("#select-result").empty();
+        result.append($('input[name="radio"]:checked').val());
+//        switch(usefulVars["Instr"])
+//            {
+//                case "NG3":
+//                    result.append("NG3");
+//                break;
+//                case "NG7":
+//                    result.append("NG7");
+//                break;
+//                case "NGB":
+//                    result.append("NGB");
+//                break;
+//                default:
+//                console.log("Something went wrong");
+//        }
+//        console.log("result = " + result);
       
     }
 
