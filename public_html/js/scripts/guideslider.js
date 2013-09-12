51c45687746c2137ca48e0c544ea2e960aab7c2e
matthew-was <Matthew@h121156.ncnr.nist.gov>
2013-09-12 16:18:21 -0400

    Build to 9/12/13

--- public_html/js/scripts/guideslider.js
+++ public_html/js/scripts/guideslider.js
@@ -7,7 +7,9 @@ var usefulVars = {
         detMin:133,
         detMax:1377,
         offset:0,
-        sampPosn:"Chamber"
+        sampPosn:"Chamber",
+        sampAp:12.7,
+        sourceAp:50.0
     };
     
     function getVariables() {
@@ -16,13 +18,15 @@ var usefulVars = {
         usefulVars["detector"]= $( "#detector-slider" ).slider( "value" );
         usefulVars["offset"]= $( "#offset-slider" ).slider( "value" );
         usefulVars["sampPosn"]= $('input[name="sampPosn"]:checked').val();
+        usefulVars["sampAp"]= parseFloat($('select[id="sampAp"]').val());
     };
 
-    function handleClick(myRadio) {
+    function instClick(myRadio) {
         usefulVars["Instr"] = $('input[name="radio"]:checked').val();
         var result = $("#select-result").empty();
         result.append($('input[name="radio"]:checked').val());
-        resetInstrument();    }
+        resetInstrument();
+    }
     
     function resetInstrument() {
         getVariables ();
@@ -33,6 +37,7 @@ var usefulVars = {
                 usefulVars["detMax"] = 1377;
                 document.getElementsByName("sampPosn")[0].disabled=false;
                 document.getElementsByName("sampPosn")[1].disabled=false;
+                sourceApChange();
                 break;
             case "NG7":
                 usefulVars["guidesMax"] = 8;
@@ -40,6 +45,7 @@ var usefulVars = {
                 usefulVars["detMax"] = 1531;
                 document.getElementsByName("sampPosn")[0].disabled=false;
                 document.getElementsByName("sampPosn")[1].disabled=false;
+                sourceApChange();
                 break;
             case "NGB":
                 usefulVars["guidesMax"] = 2;
@@ -47,6 +53,7 @@ var usefulVars = {
                 usefulVars["detMax"] = 525;
                 document.getElementsByName("sampPosn")[0].disabled=true;
                 document.getElementsByName("sampPosn")[1].disabled=true;
+                sourceApChange();
                 break;
             default:
                 console.log("Something went wrong");
@@ -61,9 +68,6 @@ var usefulVars = {
             max:usefulVars["guidesMax"],
             slide: function( event, ui ) {
                 $( "#guides" ).val( ui.value );
-            },
-            change: function(event, ui) {
-                getVariables();
             }
         });
         $( "#guides" ).val( $( "#guides-slider" ).slider( "value" ) );
@@ -77,14 +81,84 @@ var usefulVars = {
             max:usefulVars["detMax"],
              slide: function( event, ui ) {
                 $( "#detector" ).val( ui.value );
-            },
-            change: function(event, ui) {
-                getVariables();
             }
         });
         $( "#detector" ).val( $( "#detector-slider" ).slider( "value" ) );
     }
+
+    function sampApClick(){
+        var check = $('select[id="sampAp"]').val();
+        var manApVal = document.getElementById("manAp");
+        if (check === "Other") {
+            manApVal.disabled = false;
+            manApVal.value="10";
+            usefulVars["sampAp"]= parseFloat(manApVal.value);
+        } else {
+            usefulVars["sampAp"]= parseFloat($('select[id="sampAp"]').val());
+            manApVal.disabled = true;
+            manApVal.value="";
+        }
+    }
+   
+    function manApChange(){
+        var manApVal = document.getElementById("manAp");
+        if(isNaN(parseFloat(manApVal.value)) === true ){
+            manApVal.value=10;
+            usefulVars["sampAp"] = manApVal.value;
+        } else {
+            usefulVars["sampAp"] = manApVal.value;
+        }
+    };
     
+    function sourceApChange(){
+        getVariables();
+        switch (usefulVars["Instr"]) {
+            case "NG3":
+                switch (usefulVars["guides"]) {
+                    case 0:
+                        $("#sourceAp").html('<option value="14.3">14.3mm</option><option value="25.4">25.4mm</option><option value="38.1">38.1mm</option>');
+                        usefulVars["sourceAp"]=$("#sourceAp").val();
+                        break
+                    case 7:
+                        $("#sourceAp").html('<option value="50.0">50.0mm</option><option value="25.0">25.0mm</option><option value="9.5">9.5mm</option>');
+                        usefulVars["sourceAp"]=$("#sourceAp").val();
+                        break
+                    default:
+                        $("#sourceAp").html('<option value="50.0">50.0mm</option>');
+                        usefulVars["sourceAp"]=$("#sourceAp").val();
+                        break
+                }
+                break
+            case "NG7":
+               switch (usefulVars["guides"]) {
+                    case 0:
+                        $("#sourceAp").html('<option value="14.3">14.3mm</option><option value="22.2">22.2mm</option><option value="38.1">38.1mm</option>');
+                        usefulVars["sourceAp"]=$("#sourceAp").val();
+                        break
+                    default:
+                        $("#sourceAp").html('<option value="50.8">50.8mm</option>');
+                        usefulVars["sourceAp"]=$("#sourceAp").val();
+                        break    
+                }
+                break
+            case "NGB":
+               switch (usefulVars["guides"]) {
+                    case 0:
+                        $("#sourceAp").html('<option value="13.0">13.0mm</option><option value="25.0">25.0mm</option><option value="38.0">38.0mm</option>');
+                        usefulVars["sourceAp"]=$("#sourceAp").val();
+                        break
+                    default:
+                        $("#sourceAp").html('<option value="50.0">50.0mm</option>');
+                        usefulVars["sourceAp"]=$("#sourceAp").val();
+                        break    
+                }
+                break
+        }
+    };
+    
+    function sourceApClick() {
+        usefulVars["sourceAp"]=$("#sourceAp").val();
+    };
     
     $(function(){
         $("#radio").buttonset();
@@ -100,6 +174,7 @@ var usefulVars = {
         },
         change: function(event, ui) {
             getVariables();
+            sourceApChange();
         }
         });
         $( "#guides" ).val( $( "#guides-slider" ).slider( "value" ) );
@@ -114,6 +189,7 @@ var usefulVars = {
                     value:$( "#guides" ).spinner( "value" )
                 });
                 getVariables();
+                sourceApChange();
             },
             change: function (event, ui ) {
                 checkVal= $( "#guides" ).spinner( "value" );
@@ -127,10 +203,11 @@ var usefulVars = {
                     });
                 }
                 getVariables();
+                sourceApChange();
             }
         });
     });
-
+    
        $(function() {
         $( "#detector-slider" ).slider({        
         value: usefulVars["detector"],
@@ -213,4 +290,8 @@ var usefulVars = {
                 getVariables();
             }
         });
+    });
+    
+    $(function() {
+        $("#sourceAp").html('<option value="5.00">5.00cm</option>');
     });
